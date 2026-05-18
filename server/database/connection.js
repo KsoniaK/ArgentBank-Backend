b282a18d9f53ee2ecdad47d6e120563531f82e4e
sonia kechit <kechits@yahoo.com>
2026-05-18 22:12:07 +0200

    connect mongodb atlas

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,21 +1,38 @@
-// Ce fichier : ouvrir la connexion entre l’application backend et la base de données au démarrage (MongoDB).
+// // Ce fichier : ouvrir la connexion entre l’application backend et la base de données au démarrage (MongoDB).
 
-// Mongoose = bibliothèque Node.js qui sert à :
-  // se connecter à MongoDB,
-  // définir des schémas et des modèles,
-  // manipuler les données plus facilement.
-const mongoose = require('mongoose');
-// on définit l’URL de connexion à la base.
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+// // Mongoose = bibliothèque Node.js qui sert à :
+//   // se connecter à MongoDB,
+//   // définir des schémas et des modèles,
+//   // manipuler les données plus facilement.
+// const mongoose = require('mongoose');
+// // on définit l’URL de connexion à la base.
+// const databaseUrl =
+//   process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
 
-// Ce modeul exporte une fonction qu’on pourra appeler au lancement de l’application, par exemple dans server.js ou app.js
+// // Ce modeul exporte une fonction qu’on pourra appeler au lancement de l’application, par exemple dans server.js ou app.js
+// module.exports = async () => {
+//   try {
+//     await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+//     console.log('Database successfully connected')
+//   } catch (error) {
+//     console.error(`Database Connectivity Error: ${error}`)
+//     throw new Error(error)
+//   }
+// }
+
+
+const mongoose = require('mongoose')
+
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    console.log('Database connected')
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error('Database Connectivity Error:', error)
+    process.exit(1)
   }
 }
